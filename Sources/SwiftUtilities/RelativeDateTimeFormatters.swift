10dd1174ef230bc58e289f2647e8e2d0573ac86d
Christian Mitteldorf <christian.km@me.com>
2024-04-26 11:50:24 +0200

    Add relativeTo parameter for RelativeDateTimeFormatter

--- Sources/SwiftUtilities/RelativeDateTimeFormatters.swift
+++ Sources/SwiftUtilities/RelativeDateTimeFormatters.swift
@@ -10,9 +10,9 @@ import Foundation
 @available(macOS 10.15, iOS 13.0, *)
 extension RelativeDateTimeFormatter {
 
-    public class func full(_ date: Date) -> String {
+    public class func full(_ date: Date, relativeTo: Date = .now) -> String {
         let formatter = RelativeDateTimeFormatter()
         formatter.unitsStyle = .full
-        return formatter.localizedString(for: date, relativeTo: .now)
+        return formatter.localizedString(for: date, relativeTo: relativeTo)
     }
 }
--- Tests/SwiftUtilitiesTests/DateFormatterTests.swift
+++ Tests/SwiftUtilitiesTests/DateFormatterTests.swift
@@ -25,7 +25,7 @@ class DateFormatterTests: XCTestCase {
     @available(iOS 13.0, *)
     func testFullRelativeFormatter() throws {
         let date = try XCTUnwrap(Date(day: 6, month: 7, year: 2018))
-        let sut = RelativeDateTimeFormatter.full(date)
+        let sut = RelativeDateTimeFormatter.full(date, relativeTo: Date(day: 6, month: 7, year: 2021)!)
         XCTAssertEqual(sut, "3 years ago")
     }
 }
